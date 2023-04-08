651109d0d3815cf767d3d38d54bec96aabda7c86
Chris <gh0st0x9@google.com>
2023-04-08 13:17:29 +1000

    register-ui finished

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -10,6 +10,8 @@ import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
+import { Toast, toast } from 'react-hot-toast';
+import Button from '../Button';
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
@@ -36,49 +38,85 @@ const RegisterModal = () => {
                 registerModal.onClose();
             })
             .catch((error) => {
-                console.log(error);
+                toast.error('Error: ' + error.message);
             })
             .finally(() => {
                 setIsLoading(false);
             });
     };
 
-    const bodyContent = <div className="flex flex-col gap-4">
-        <Heading 
-            title="Welcome to Airbnb"
-            subtitle="Register to get started"
-            center
+    const bodyContent = (
+        <div className="flex flex-col gap-4">
+            <Heading
+                title="Welcome to Airbnb"
+                subtitle="Register to get started"
+                center
+            />
+            <Input
+                id="email"
+                label="Email"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+            <Input
+                id="name"
+                label="Name"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+            <Input
+                type="password"
+                id="password"
+                label="Password"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+        </div>
+    );
+    const footerContent = (
+        <div className="flex flex-col gap-4 mt-3">
+            <hr />
+            <Button
+                outline
+                label="Continue with Google"
+                icon={FcGoogle}
+                onClick={() => {}}
+            />
+            <Button
+                outline
+                label="Continue with Github"
+                icon={AiFillGithub}
+                onClick={() => {}}
+            />
+            <div
+                className="
+            text-neutral-500
+            text-center
+            mt-4
+            font-light
+            "
+            >
+                <div className='justify-center flex flex-row items-center gap-2'>
+                    <div>Already have an account?</div>
+                    <div
+                        onClick={registerModal.onClose}
+                        className='
+                        text-neutral-800
+                        cursor-pointer
+                        hover:underline
 
-        />
-        <Input
-            id="email"
-            label="Email"
-            disabled={isLoading}
-            register={register}
-            errors={errors}
-            required
-        />
-                <Input
-            id="name"
-            label="Name"
-            disabled={isLoading}
-            register={register}
-            errors={errors}
-            required
-        />
-        <Input
-        type='password'
-            id="password"
-            label="Passowrd"
-            disabled={isLoading}
-            register={register}
-            errors={errors}
-            required
-        />
-
-
-    
-    </div>;
+                        '
+                    >Log in</div>
+                </div>
+            </div>
+        </div>
+    );
 
     return (
         <Modal
@@ -89,6 +127,7 @@ const RegisterModal = () => {
             actionLabel="Continue"
             onSubmit={handleSubmit(onSubmit)}
             body={bodyContent}
+            footer={footerContent}
         />
     );
 };
