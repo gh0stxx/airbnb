2ff689ff1d583ac6c763a231c26c94fb63354c37
Chris <gh0st0x9@google.com>
2023-04-08 22:39:00 +1000

    location module

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -13,9 +13,11 @@ import Input from '../inputs/Input';
 import { Toast, toast } from 'react-hot-toast';
 import Button from '../Button';
 import { signIn } from 'next-auth/react';
+import useLoginModal from '@/app/hooks/useLoginModal';
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
+    const loginModal = useLoginModal();
     const [isLoading, setIsLoading] = useState(false);
 
     const {
@@ -45,6 +47,11 @@ const RegisterModal = () => {
                 setIsLoading(false);
             });
     };
+
+    const onToggle = useCallback(() => {
+        registerModal.onClose();
+            loginModal.onOpen();
+      }, [registerModal, loginModal])
 
     const bodyContent = (
         <div className="flex flex-col gap-4">
@@ -106,7 +113,7 @@ const RegisterModal = () => {
                 <div className='justify-center flex flex-row items-center gap-2'>
                     <div>Already have an account?</div>
                     <div
-                        onClick={registerModal.onClose}
+                        onClick={onToggle}
                         className='
                         text-neutral-800
                         cursor-pointer
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import './globals.css';
 import ToasterProvider from './providers/ToasterProvider';
 import LoginModal from './components/modals/LoginModal';
 import { getCurrentUser } from './actions/getCurrentUser';
+import RentModal from './components/modals/RentModal';
 
 
 
@@ -30,6 +31,7 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <RentModal />
           <RegisterModal />
           <LoginModal />
           <Navbar currentUser={currentUser}/>
