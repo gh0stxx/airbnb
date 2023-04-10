67eba066352c959f0e6cae5442281cd49bb2b5f5
Chris <gh0st0x9@google.com>
2023-04-10 21:15:57 +1000

    few changes

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,20 +1,27 @@
-import bcrypt from 'bcrypt';
-import prisma from '@/app/libs/prismadb';
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
 
-export async function POST(request: Request) {
-    const body = await request.json();
-    const { email, name, password } = body;
+import prisma from "@/app/libs/prismadb";
 
-    const hashedPassword = await bcrypt.hash(password, 12);
+export async function POST(
+  request: Request, 
+) {
+  const body = await request.json();
+  const { 
+    email,
+    name,
+    password,
+   } = body;
 
-    const user = await prisma.user.create({
-        data: {
-            email,
-            name,
-            hashedPassword,
-        },
-    });
+   const hashedPassword = await bcrypt.hash(password, 12);
 
-    return NextResponse.json(user);
+   const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      hashedPassword,
+    }
+  });
+
+  return NextResponse.json(user);
 }
