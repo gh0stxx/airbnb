f5d7d0b7722c0cc21c31e6dc5f0e40b1a80b839e
Chris <gh0st0x9@google.com>
2023-04-10 23:11:12 +1000

    test

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import ClientOnly from "./components/ClientOnly";
 interface HomeProps {
   searchParams: IListingsParams
 };
-export const dynamic = 'force-dynamic'
+//export const dynamic = 'force-dynamic'
 
 const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
