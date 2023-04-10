ce32c6831301256ff18252a92a5091fe5edde2dc
Chris <gh0st0x9@google.com>
2023-04-10 17:07:38 +1000

    listings and category

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { safeUser } from '../types';
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import useFavorite from '../hooks/useFavorite';
 
 interface HeartButtonProps {
     listingId: string;
@@ -13,8 +14,14 @@ const HeartButton: React.FC<HeartButtonProps> = ({
     listingId,
     currentUser,
 }) => {
-    const hasFavorited = false;
-    const toggleFavorite = () => {};
+    
+    const {hasFavorited, toggleFavorite} = useFavorite({
+        listingId,
+        currentUser
+    });
+    
+
+
     return (
         <div
             onClick={toggleFavorite}
@@ -26,7 +33,7 @@ const HeartButton: React.FC<HeartButtonProps> = ({
             />
 
             <AiFillHeart 
-                size={28}
+                size={24}
                 className={
                     hasFavorited ? 'fill-rose-500' : 'fill-neutral-500/70'
                 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import getCurrentUser from "./actions/getCurrentUser";
-import getListings from "./actions/getListings";
-import ClientOnly from "./components/ClientOnly";
-import Container from "./components/Container";
-import EmptyState from "./components/EmptyState";
-import ListingCard from "./components/listings/ListingCard";
+import getCurrentUser from './actions/getCurrentUser';
+import getListings from './actions/getListings';
+import ClientOnly from './components/ClientOnly';
+import Container from './components/Container';
+import EmptyState from './components/EmptyState';
+import ListingCard from './components/listings/ListingCard';
 
 export default async function Home() {
     const listings = await getListings();
@@ -11,37 +11,30 @@ export default async function Home() {
 
     if (listings.length === 0) {
         return (
-          <ClientOnly>
-            <EmptyState showReset />
-          </ClientOnly>
+            <ClientOnly>
+                <EmptyState showReset />
+            </ClientOnly>
         );
     }
 
     return (
         <ClientOnly>
             <Container>
-                <div className="pt-24 grid grid-cols-1
-                sm:grid-cols-2
-                md:grid-cols-3
-                lg:grid-cols-4
-                xl:grid-cols-5
-                2xl:grid-cols-6
-                gap-8
-                ">
-                    {listings.map((listing: any) => {
+                <div
+                    className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3
+                         lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8"
+                >
+                    {listings.map((listing) => {
                         return (
-
                             <ListingCard
                                 key={listing.id}
                                 data={listing}
                                 currentUser={currentUser}
-                                />
-                            
-                        )
-                        })}
-
+                            />
+                        );
+                    })}
                 </div>
             </Container>
         </ClientOnly>
-    )
+    );
 }
